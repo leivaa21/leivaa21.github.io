2bdb297efb36347d38afd4105364c4363bf037cd
leivaa <adrianleiva212@gmail.com>
2022-10-24 18:12:00 +0200

    feat: updated info for home page

--- src/components/files/info.tsx
+++ src/components/files/info.tsx
@@ -3,6 +3,13 @@ const Info = () => {
   <>
     <h1>Hello World</h1>
     <h2>I&apos;m Leivaa</h2>
+    <p>
+      I&apos;m a <b>Software/Backend Developer</b> and Learning enthusiast.
+      <br/>Always looking forward develop new skills and learn new technologies.
+      <br/>Currently finishing my engineering degree at <a href="https://www.uco.es" target="_blank" rel="noreferrer"><b>UCO</b></a>{' '}
+      and working with the <a href="https://www.uco.es/aulasoftwarelibre/" target="_blank" rel="noreferrer"><b>Aula de Software Libre</b></a> as Infrastructure Delegate.
+      <br/>I&apos;m also working at <a href="https://genial.ly" target="_blank" rel="noreferrer"><b>Genial.ly</b></a> as Software Developer Trainee.
+    </p>
   </>
   )
 }
